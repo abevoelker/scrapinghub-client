25147d86fb98ad505a973848ac685b659aa6a3d7
Abe Voelker <abe@abevoelker.com>
2015-07-25 14:30:51 -0500

    Allow code coverage reports to be sent to Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.configure_rspec_metadata!
   config.hook_into :webmock
+  # allow code coverage reports to be sent to Code Climate
+  config.ignore_hosts "codeclimate.com"
 end
 
 RSpec.configure do |c|
